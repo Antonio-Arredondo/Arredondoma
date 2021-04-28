58e8225358c218e9885c470a659b743553032960
Antonio-Arredondo <antonioarredondo06@gmail.com>
2021-04-28 08:30:16 -0500

    update

---
+++ Parcial2/Login/login.js
@@ -0,0 +1,33 @@
+$('document').ready(function(){
+    $('#boton').click(validate)
+});
+
+function validate(){
+
+    var un = $("#usuario").val();
+    var pass = $("#contraseña").val();
+    
+    if(un == "")
+    {   
+        alert("Ingresa un usuario!");
+
+    }else if(pass == "")
+    {
+        alert("Ingrese su contraseña");
+
+    }else if(un.length < 6){
+        alert("El usuario debe contener 6 Caracteres!")
+
+    } else if(pass.length <= 7){
+        alert("Contraseña demasiado corta!")
+
+    } else if(un=="Antonio" && pass=="12345678")
+    {   
+        alert("Iniciando Sesion...");
+
+    } else{
+        alert("Ingrese datos validos!")
+
+    }
+    
+}
