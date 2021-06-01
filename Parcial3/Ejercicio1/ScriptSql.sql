c81d76305fb9b3bc6e2f57cc2c5fd1e8da007c51
Antonio Arredondo <33409111+Antonio-Arredondo@users.noreply.github.com>
2021-06-01 18:31:54 -0500

    update

---
+++ Parcial3/Ejercicio1/ScriptSql.sql
@@ -0,0 +1,28 @@
+create database Arredondoma
+use Arredondoma
+
+create table tabUsuarios
+(
+    idUsuario smallint not null auto_increment,
+    nombre varchar(30),
+    apPaterno varchar(30),
+    apMaterno varchar(30),
+    usuario varchar(20),
+    contrasena varchar(30),
+    primary key(idUsuario)
+);
+
+insert into tabUsuarios(nombre,apPaterno,apMaterno,usuario,contrasena)
+values('Antonio','Arredondo','Martinez','Usuario1','Contrasena1',)
+
+insert into tabUsuarios(nombre,apPaterno,apMaterno,usuario,contrasena)
+values('Alondra','Suazo','Alvardo','Usuario2','Contrasena2',)
+
+insert into tabUsuarios(nombre,apPaterno,apMaterno,usuario,contrasena)
+values('Efrain','Arredondo','Suazo','Usuario3','Contrasena3',)
+
+insert into tabUsuarios(nombre,apPaterno,apMaterno,usuario,contrasena)
+values('Antonio','Arredondo','Suazo','Usuario4','Contrasena4',)
+
+insert into tabUsuarios(nombre,apPaterno,apMaterno,usuario,contrasena)
+values('Luis','Arredondo','Martinez','Usuario5','Contrasena5',)
